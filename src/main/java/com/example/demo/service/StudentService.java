e6819d68bd13b8d48adedd2980036a9319b4d18e
bala-amypo <bala@amypo.in>
2025-12-18 05:25:01 +0000

    User commit

--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -3,8 +3,9 @@ package com.example.demo.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.stereotype.Service;
 import com.example.demo.entity.Student;
-
+@Service
 public interface StudentService {
     
     Student insertStudent(Student st);
